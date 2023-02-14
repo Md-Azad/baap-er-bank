7c022860e64fe96eec18f7648df53841aa73d91b
Md-Azad <sokalazad@gmail.com>
2023-02-14 12:30:15 +0100

    added deposit calculaiton section

---
+++ js/deposit.js
@@ -0,0 +1,19 @@
+document.getElementById('btn-deposit').addEventListener('click',function(){
+    // fatching the user deposit value;
+    const userDeposit = document.getElementById('user-deposit');
+    const deposit = userDeposit.value;
+    // set deposit value on deposit section.
+    const depositSection = document.getElementById('total-deposit');
+    const totalDeposit = depositSection.innerText;
+
+    const total = parseFloat(deposit) +parseFloat(totalDeposit);
+    depositSection.innerText =total;
+    userDeposit.value = '';
+
+    // add the deposit value to balance section.
+    const currentValue = document.getElementById('previous-balance');
+    const currentBalance = currentValue.innerText;
+    const updatedBalance = parseFloat(deposit) + parseFloat(currentBalance);
+    currentValue.innerText = updatedBalance;
+})
+
---
+++ js/withdraw.js
@@ -0,0 +1,3 @@
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    // get the withdraw amount for the withdraw field
+})
